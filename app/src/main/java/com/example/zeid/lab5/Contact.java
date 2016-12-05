06e9455ed91dba93da093a9297489a680b435e7f
mohamedAbouzeid <mohamed.amr-abouzeid@student.guc.edu.eg>
2016-12-05 20:36:32 +0100

    contact details and price added

---
+++ app/src/main/java/com/example/zeid/lab5/Contact.java
@@ -0,0 +1,15 @@
+package com.example.zeid.lab5;
+
+/**
+ * Created by zeid on 05/12/16.
+ */
+
+public class Contact {
+    public String name;
+    public int number;
+
+    public Contact(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
+}
